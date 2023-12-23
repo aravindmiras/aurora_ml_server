3a01f6f12c2beb8e0bb95070b9fb076d4bf17f93
aravindmiras <aravindmiras@gmail.com>
2023-12-23 13:00:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { rdb } from "./firebaseConfig";
+import {ref, onValue, set} from 'firebase/database';
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [dataone, setData] = useState([]);
+
+  const [voltage, setVoltage]=useState('0');
+  const [rated_voltage, setRV]=useState('0');
+
+  const [rated_current, setRC]=useState('0');
+  const [current, setCurrent]=useState('0');
+  const [rpm, setRPM]=useState('0');
+  const [temperature, setTemperature]=useState('0');
+  const [humidity, setHumidity]=useState('0');
+  const [power, setPower] = useState('0');
+  const [vibration, setVibration]=useState('0');
+  const [mlurl, setMLurl]=useState('');
+  const [status, setStatus]=useState('No maintenance required')
+  const [trigger, setTrigger]=useState(0);
+
+
+  const mlCode = async  () => {
+  const params = { current,voltage,temperature, humidity, vibration };
+   await axios
+      .post('https://bb7a-14-194-54-206.ngrok-free.app/prediction', params,{
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        }
+      })
+      .then((res) => {
+        const msg = res.data.data;
+        console.log(msg);
+        setStatus(msg);
+      });
+      
+      // .catch((error) => alert(`Error: ${error.message} Unprocessable data`));
+  };
+
+  // const set = (msg) => {
+  //   setStatus(msg);
+  // };
+  const firebaseGet = () =>{
+    const fetchRef = ref(rdb,'motordat/');
+    onValue(fetchRef, (snapshot) =>{
+      const adata = snapshot?.val();
+      const motorData = Object.keys(adata).map(key => ({
+         id: key,
+         ...adata[key] 
+      }));
+      setData(motorData);
+      if(dataone.length > 0){
+        setVoltage(dataone[0]?.actualVoltage);
+        setRC(dataone[0]?.ratedCurrent);
+        setRV(dataone[0]?.ratedVoltage);
+        setCurrent(dataone[0]?.actualCurrent);
+        setRPM(dataone[0]?.actualRPM);
+        setTemperature(dataone[0]?.temperature);
+        setHumidity(dataone[0]?.humidity);
+        setVibration(dataone[0]?.vibration);
+        setPower(dataone[0]?.actualVoltage*dataone[0]?.actualCurrent);
+        setMLurl(dataone[0]?.mlmodel);
+        console.log('setValues method called');
+        
+      }
+    }
+    );
+  }
+
+  const firebasePush = () =>{
+    const pushRef = ref(rdb, 'motordat/motor/maintenance/');
+    set(pushRef, status);
+    console.log('firebase push called');
+  }
+
+  useEffect(()=>{
+    
+    const timeout = setTimeout(() => {
+      firebaseGet();
+      mlCode();
+      firebasePush();
+      setTrigger((trigger) => trigger+1)
+      
+    }, 5000);
+    
+          
+    
+  
+  
+  return () => clearTimeout(timeout);
+},[trigger]);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Aurora ML Admin - {trigger}</h1>
+      <p>Voltage: {voltage}/{rated_voltage}</p>
+      <p>Current: {current}/{rated_current} </p>
+      <p>RPM: {rpm}</p>
+      <p>Power: {power}</p>
+      <p>Temperature: {temperature}</p>
+      <p>Humidity: {humidity}</p>
+      <p>Status: {JSON.stringify(status)}</p>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { getDatabase } from 'firebase/database';
+import firebase from 'firebase/compat/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC4MDz3X6aFscHSYYUQJh2oBfjBPEy-lYU",
+  authDomain: "esp-demo-17ed1.firebaseapp.com",
+  databaseURL: "https://esp-demo-17ed1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "esp-demo-17ed1",
+  storageBucket: "esp-demo-17ed1.appspot.com",
+  messagingSenderId: "265923535250",
+  appId: "1:265923535250:web:7c05882460f47b8682214b"
+}
+
+
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const rdb= getDatabase();
